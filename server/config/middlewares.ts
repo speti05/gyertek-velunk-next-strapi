dccee924db04d0d00a08142025bb462a8ffba7bc
speti05 <speti05@gmail.com>
2026-09-09 10:04:53 +0100

    * syntax fix

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -4,25 +4,25 @@
 import { getClientUrl } from "../src/lib/config/client-url";
 
 // process.env object, not a callable helper.
-export default ( () => [
-  'strapi::logger',
-  'strapi::errors',
+export default () => [
+  "strapi::logger",
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'frame-src': ["'self'", getClientUrl()],
+          "frame-src": ["'self'", getClientUrl()],
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
